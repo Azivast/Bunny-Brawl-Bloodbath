b8a902263ee8f36a4a11069329abda6d6d9f6aea
Azivast <olle.astre@tutamail.com>
2025-01-25 13:22:20 +0100

    Stop enemies from shooting through walls

--- Assets/Enemies/EnemyWeaponHandler.cs
+++ Assets/Enemies/EnemyWeaponHandler.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using UnityEngine;
 
 public class EnemyWeaponHandler : MonoBehaviour
 {
     [SerializeField] private GameObject weapon;
     [SerializeField] private Transform weaponPosition;
-    [SerializeField] private float atttackRange;
+    [SerializeField] private float attackRange = 3;
+    [SerializeField] LayerMask raycastHitLayers;
     
     private WeaponBehaviour weaponBehaviour;
     private int bulletLayer = 11;
@@ -20,21 +22,30 @@ public class EnemyWeaponHandler : MonoBehaviour
 
     private bool TryFindTarget(out TargetBehaviour target) {
         int mask = LayerMask.GetMask("Player");
-        Collider2D col = Physics2D.OverlapCircle(transform.position, atttackRange, mask);
+        Collider2D col = Physics2D.OverlapCircle(transform.position, attackRange, mask);
 
-        if (col == null) {
-            target = null;
-            return false;
+        if (col !=null && col.TryGetComponent<TargetBehaviour>(out var playerTarget))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, (playerTarget.transform.position-transform.position).normalized, attackRange, raycastHitLayers);
+            if (hit.transform.TryGetComponent<PlayerBehaviour>(out _))
+            { 
+                Debug.DrawRay(transform.position, (playerTarget.transform.position-transform.position).normalized * attackRange, Color.red); 
+                target = playerTarget;
+                return true;
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, (playerTarget.transform.position-transform.position).normalized * attackRange, Color.yellow); 
+            }
         }
-        else {
-            target = col.GetComponent<TargetBehaviour>();
-            return true;
-        }
+
+        target = null;
+        return false;
     }
 
     private void FixedUpdate() {
         if (TryFindTarget(out TargetBehaviour target)) {
-            weaponBehaviour.Target = target.transform.position;
+            weaponBehaviour.Target = target.transform.position + UnityEngine.Random.insideUnitSphere;// * (weaponBehaviour.Target-target.transform.position).magnitude;
             weaponBehaviour.Shoot(bulletLayer);
         }
     }
