e7bac195ad6e97754ecb375f5580a9e6c1708a08
LinnéaSkoldator <linnea@klangby.com>
2025-01-28 11:03:56 +0100

    Footstep sound

---
+++ Assets/Audio/Sfx/Footsteps/FootSteps.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FootSteps : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> footStepsClip;
+    [SerializeField]private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayFootStep()
+    {
+        audioSource.PlayOneShot(footStepsClip[Random.Range(0, footStepsClip.Count)]);
+        Debug.Log("Footstep");
+    }
+}
