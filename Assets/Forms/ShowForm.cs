cdadeb5f4a9dc6e0e6711218a4e7d37d601841a0
Linnéa <linnea@klangby.com>
2025-02-12 20:49:30 +0100

    possible to randomize questions

--- Assets/Forms/ShowForm.cs
+++ Assets/Forms/ShowForm.cs
@@ -23,6 +23,8 @@ public class ShowForm : MonoBehaviour
     [SerializeField] private GameObject Answers;
     [SerializeField] private GameObject NextButton;
     [SerializeField] private GameObject PreviousButton;
+    [Tooltip("Keeps the first and last prompt in same place, randomize the rest.")]
+    [SerializeField] private bool randomOrder;
     private int index = 0;
 
     private void Start()
@@ -30,10 +32,12 @@ public class ShowForm : MonoBehaviour
         textPrompt.text = questionsAndPrompts[0].QuestionText;
         Answers.SetActive(questionsAndPrompts[index].CanBeAnswered);
         PreviousButton.SetActive(false);
+        if(randomOrder)RandomizeOrder(questionsAndPrompts);
     }
 
     public void Next()
     {
+        if(questionsAndPrompts[index].buttonSelected == null && questionsAndPrompts[index].CanBeAnswered)return;
         index++;
         textPrompt.text = questionsAndPrompts[index].QuestionText;
         Answers.SetActive(questionsAndPrompts[index].CanBeAnswered);
@@ -63,5 +67,16 @@ public class ShowForm : MonoBehaviour
         questionsAndPrompts[index].AnswerText = 
             EventSystem.current.currentSelectedGameObject.GetComponentInChildren<TMP_Text>().text;
         questionsAndPrompts[index].buttonSelected = EventSystem.current.currentSelectedGameObject;
+        
+    }
+
+    private void RandomizeOrder<T>(List<T> ts)
+    {
+        var count = ts.Count;
+        var last = count - 2;
+        for (var i = 1; i < last; ++i) {
+            var r = UnityEngine.Random.Range(i, count - 1);
+            (ts[i], ts[r]) = (ts[r], ts[i]);
+        }
     }
 }
